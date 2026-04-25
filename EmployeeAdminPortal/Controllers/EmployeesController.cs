55a63b7301454a985f2c4e4aa015c46d30013e0f
Nick O'Connor <nioc4468@colorado.edu>
2026-04-25 15:29:23 -0500

    Created POST request for adding Employees

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -1,4 +1,6 @@
 ﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +18,25 @@ namespace EmployeeAdminPortal.Controllers {
         public IActionResult GetAllEmployees() {
             var allEmployees = dbContext.Employees.ToList();    // Conect to DB (dbContext) and return all elements of table
             return Ok(allEmployees);                            // Since this is an HTTP request we have to 'send a 200 response'
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto) {
+            // We use DTO (Data Transfer Object) here because it abstracts what code is visible on either side of our database.
+            // It doesn't matter much in this scenario but it allows more modularity
+            var employeeEntity = new Employee() {               
+                Name    = addEmployeeDto.Name,
+                Email   = addEmployeeDto.Email,
+                Salary  = addEmployeeDto.Salary,
+                Phone   = addEmployeeDto.Phone,
+                Notes   = addEmployeeDto.Notes,
+                Address = addEmployeeDto.Address,
+            };
+
+            dbContext.Employees.Add(employeeEntity);            // This stages changes but doesn't modify the DB itself
+            dbContext.SaveChanges();                            // THIS is what actually makes our changes real
+
+            return Ok(employeeEntity);
         }
     }
 }
---
+++ EmployeeAdminPortal/Models/AddEmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeAdminPortal.Models {
+    public class AddEmployeeDto {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required decimal Salary { get; set; }
+        public string? Phone { get; set; }
+        public string? Notes { get; set; }
+        public string? Address { get; set; }
+    }
+}
