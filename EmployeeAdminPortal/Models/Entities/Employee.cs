b79dff442b3db1b195e15dbb2c0c390c976ddb98
Nick O'Connor <nioc4468@colorado.edu>
2026-04-25 14:18:25 -0500

    Added Employee class file

---
+++ EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeAdminPortal.Models.Entities {
+    public class Employee {
+
+        public Guid Id { get; set; } // Guid = Globally Unique ID
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required decimal Salary { get; set; }
+        public string? Phone { get; set; }
+        public string? Notes { get; set; }
+
+        public string? Address { get; set; }
+    }
+}
