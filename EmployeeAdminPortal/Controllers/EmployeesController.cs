b2d26e260bc1b61a0d1e70b3e19b8be070a05a8e
Nick O'Connor <nioc4468@colorado.edu>
2026-04-25 15:09:45 -0500

    Added first GET request

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers {
+    [Route("api/[controller]")] // <-- localhost:XXXX/api/employees
+    [ApiController]
+    public class EmployeesController : ControllerBase {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext) {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees() {
+            var allEmployees = dbContext.Employees.ToList();    // Conect to DB (dbContext) and return all elements of table
+            return Ok(allEmployees);                            // Since this is an HTTP request we have to 'send a 200 response'
+        }
+    }
+}
