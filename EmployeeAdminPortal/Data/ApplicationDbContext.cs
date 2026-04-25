bf43edf579547bad95d8bcac79c749acbc35e36e
Nick O'Connor <nioc4468@colorado.edu>
2026-04-25 14:25:39 -0500

    Created DbContext

---
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeAdminPortal.Models.Entities;
+
+namespace EmployeeAdminPortal.Data {
+    public class ApplicationDbContext : DbContext {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options) {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
