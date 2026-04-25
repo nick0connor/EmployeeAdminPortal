4ddb950244310a2c1e7cdff07f380facf2aed5c6
Nick O'Connor <nioc4468@colorado.edu>
2026-04-25 15:29:43 -0500

    Removed blank space

--- EmployeeAdminPortal/Models/Entities/Employee.cs
+++ EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -7,7 +7,6 @@
         public required decimal Salary { get; set; }
         public string? Phone { get; set; }
         public string? Notes { get; set; }
-
         public string? Address { get; set; }
     }
 }
