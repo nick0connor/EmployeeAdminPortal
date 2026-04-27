affb75611edc1ddfe1fb8f99625ffcc410be6de7
Nick O'Connor <nioc4468@colorado.edu>
2026-04-27 17:48:33 -0500

    Following along angular.dev/tutorials/first-app/ tutorial

--- AngularClient/src/app/app.ts
+++ AngularClient/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [Home],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/public/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home />
+      </section>
+    </main>
+  `,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularClient');
 }
