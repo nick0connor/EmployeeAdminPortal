19b244fe84049466674038d09e48aae1cfd90f5c
Nick O'Connor <nioc4468@colorado.edu>
2026-04-29 11:07:48 -0500

    added HttpClient to providers

--- AngularClient/src/main.ts
+++ AngularClient/src/main.ts
@@ -3,7 +3,12 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import {provideRouter} from '@angular/router';
 import routeConfig from './app/routes';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(App, {
-  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+  providers: [
+    provideProtractorTestingSupport(), 
+    provideRouter(routeConfig),
+    provideHttpClient()
+  ],
 }).catch((err) => console.error(err));
