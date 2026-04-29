762ba30bd3a09faa71572698740198bd7dc1999d
Nick O'Connor <nioc4468@colorado.edu>
2026-04-29 17:00:12 -0500

    Updated routing info

--- AngularClient/src/app/routes.ts
+++ AngularClient/src/app/routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {Home} from './home/home';
 import { EmployeeDetails } from './employee-details/employee-details';
+import { CreateEmployee } from './create-employee/create-employee';
 
 const routeConfig: Routes = [
   {
@@ -13,6 +14,11 @@ const routeConfig: Routes = [
     component: EmployeeDetails,
     title: 'All Details about an employee',
     runGuardsAndResolvers: 'always'
+  },
+  {
+    path: 'new',
+    component: CreateEmployee,
+    title: 'Create New Employee'  
   }
 ];
 
