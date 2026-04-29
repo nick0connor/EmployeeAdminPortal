a6abe5913a19ebc171b6198c56f0fa46fd1b5a55
Nick O'Connor <nioc4468@colorado.edu>
2026-04-29 14:14:51 -0500

    Added Filtering

--- AngularClient/src/app/home/home.ts
+++ AngularClient/src/app/home/home.ts
@@ -3,11 +3,13 @@ import { EmployeeService } from '../employeeService';
 import { EmployeeInfo } from '../employeeInfo';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FilterEmployeeRequest } from '../filterEmployeeRequest';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -22,24 +24,19 @@ export class Home {
   pageSize: number = 10;
   pageNum: number = 1;
 
+  filterForm = new FormGroup({
+    formName: new FormControl(null),
+    formEmail: new FormControl(null),
+    formMinSal: new FormControl(null),
+    formMaxSal: new FormControl(null)
+  });
+
   constructor() {
     this.route.queryParams.subscribe(params => {
       const page = +(params['page'] ?? 1);
       this.pageNum = page;
-      this.updateEmployeeList(page);
+      this.filterResults(page);
     })
-  }
-
-  updateEmployeeList(page: number) {
-    this.employeeService.getEmployees(page, this.pageSize).subscribe({
-      next: (data) => {
-        this.filteredEmployeeList = data;
-        this.cdr.detectChanges();
-      },
-      error: (err) => {
-        console.error("Api Error: ", err);
-      }
-    });
   }
 
   changePage(delta: number) {
@@ -52,6 +49,35 @@ export class Home {
     this.router.navigate([], {
       queryParams: { page: newPage },
       queryParamsHandling: 'merge'
+    });
+  }
+
+  filterResults(page?: number | undefined){
+    const numHelper = (input: string | null | undefined): number | undefined => 
+      (input != null && input != undefined && input != "") ? Number(input) : undefined;
+    
+    const stringHelper = (input: string | null | undefined): string | undefined => 
+      (input != null && input != undefined && input != "") ? input : undefined;
+
+    const filterEmployeeRequest: FilterEmployeeRequest = {
+      name: stringHelper(this.filterForm.value.formName),
+      email: stringHelper(this.filterForm.value.formEmail),
+      minSalary: numHelper(this.filterForm.value.formMinSal),
+      maxSalary: numHelper(this.filterForm.value.formMaxSal),
+    };
+
+    if (page == null || page == undefined){
+      this.route.queryParams.subscribe(params => { page = +(params['page'] ?? 1); })
+    }
+
+    this.employeeService.getEmployeesFiltered(filterEmployeeRequest, page, this.pageSize).subscribe({
+      next: (data) => {
+        this.filteredEmployeeList = data;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error("Api Error: ", err);
+      }
     });
   }
 }
