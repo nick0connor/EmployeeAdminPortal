deb3bd728992e3b15a766a50c4f2510c296eb384
Nick O'Connor <nioc4468@colorado.edu>
2026-04-29 11:08:42 -0500

    Linked API to frontend enough for GetAllEmployees() to work

--- AngularClient/src/app/home/home.ts
+++ AngularClient/src/app/home/home.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { EmployeeService } from '../employeeService';
+import { EmployeeInfo } from '../employeeInfo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+  employeeService: EmployeeService = inject(EmployeeService);
+  employeeList: EmployeeInfo[] = [];
+  filteredEmployeeList: EmployeeInfo[] = [];
+
+  constructor() {
+    this.employeeService.getAllEmployees().subscribe({
+      next: (data) => {
+        console.log("Data: ", data);
+        this.employeeList = data;
+        this.filteredEmployeeList = data;
+      },
+      error: (err) => {
+        console.error("Api Error: ", err);
+      }
+    });
+  }
+}
