4107d9f90207f47ebec4182a05e5c2f984e84e79
Nick O'Connor <nioc4468@colorado.edu>
2026-04-29 12:13:31 -0500

    switched api call to paged - works but page wont refresh on data (ffs)

--- AngularClient/src/app/home/home.ts
+++ AngularClient/src/app/home/home.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { EmployeeService } from '../employeeService';
 import { EmployeeInfo } from '../employeeInfo';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,19 +14,44 @@ import { CommonModule } from '@angular/common';
 export class Home {
 
   employeeService: EmployeeService = inject(EmployeeService);
-  employeeList: EmployeeInfo[] = [];
+  route = inject(ActivatedRoute);
+  router = inject(Router);
+
   filteredEmployeeList: EmployeeInfo[] = [];
+  pageSize: number = 10;
+  pageNum: number = 1;
 
   constructor() {
-    this.employeeService.getAllEmployees().subscribe({
+    this.route.queryParams.subscribe(params => {
+      console.log('init')
+      const page = +(params['page'] ?? 1);
+      this.pageNum = page;
+      this.updateEmployeeList(page);
+    })
+  }
+
+  updateEmployeeList(page: number) {
+    this.employeeService.getEmployees(page, this.pageSize).subscribe({
       next: (data) => {
         console.log("Data: ", data);
-        this.employeeList = data;
         this.filteredEmployeeList = data;
       },
       error: (err) => {
         console.error("Api Error: ", err);
       }
+    });
+  }
+
+  changePage(delta: number) {
+    const currentPage = +(this.route.snapshot.queryParamMap.get('page') ?? 1);
+    const newPage = currentPage + delta;
+
+    if (newPage < 1) return;
+    if (newPage > currentPage && this.filteredEmployeeList.length < this.pageSize) return;
+
+    this.router.navigate([], {
+      queryParams: { page: newPage },
+      queryParamsHandling: 'merge'
     });
   }
 }
